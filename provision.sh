83dc5a0c3883834b33b5c36fc09d05ab4ddf8de7
Justin Azoff <justin.azoff@gmail.com>
2014-02-19 09:40:40 -0500

    initial vagrant stuff

---
+++ provision.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+yum install -y postgresql postgresql-server perl-DBD-Pg
+service postgresql initdb
+service postgresql start
+
+
+#setup db and allow vagrant user access
+su - postgres -c '
+createuser -DRS blackhole
+createdb blackhole -O blackhole
+psql blackhole < /vagrant/schema.sql
+createuser -DRS vagrant
+psql blackhole -c "grant all on database blackhole to vagrant"
+'
