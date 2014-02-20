5aa81769b668910a05189a032741bf0bcd103ec3
Justin Azoff <justin.azoff@gmail.com>
2014-02-20 11:44:06 -0500

    helper for writing files

---
+++ fileutil.pm
@@ -0,0 +1,20 @@
+package fileutil;
+use strict;
+use warnings;
+
+use base 'Exporter';
+our @EXPORT_OK = qw(write_file);
+
+sub write_file {
+	my ($filename, $contents) = @_;
+
+	my $tf = $filename . ".tmp";
+	open(my $tmp, ">", $tf) or die "Cannot open $tf: $!";
+	print $tmp, $contents;
+	close($tmp);
+	
+	rename ($tf, $filename) or die "Cannot write to $filename";
+
+}
+
+1;
