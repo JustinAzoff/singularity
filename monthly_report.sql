ab2165eefc5ce3ba72a620c4581f53df0044e7c5
Justin Azoff <justin.azoff@gmail.com>
2015-06-17 11:41:12 -0400

    Show unique + total ip counts, not just total

--- monthly_report.sql
+++ monthly_report.sql
@@ -1,8 +1,8 @@
 \echo OSSEC Blocks by month
-select month, count(1) from (select date_trunc('month', block_when) as month from blocklog where block_who='secops') as foo group by
+select month, count(distinct(ip)) as unique, count(1) as total from (select date_trunc('month', block_when) as month, block_ipaddress as ip from blocklog where block_who='secops') as foo group by
                 month order by month asc;
 
 \echo Bro Blocks by month and notice type
-select month, reason, count(1) from (select date_trunc('month', block_when) as month, (string_to_array(block_why, ' '))[1] as reason
+select month, reason, count(distinct(ip)) as unique, count(1) as total from (select date_trunc('month', block_when) as month, (string_to_array(block_why, ' '))[1] as reason, block_ipaddress as ip
                 from blocklog where block_who='bro') as foo group by month, reason order by month,reason;
 
